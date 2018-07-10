f534c5f138901e457691644e5528d1e5806da0e2
Thomas Lai <thomaslai@Thomass-iMac.local>
2018-07-10 17:08:56 +0800

    wrap with create react app

---
+++ src/App.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const { paramsForServer } = require('feathers-hooks-common');
+const { UserService } = require('./services');
+const axios = require('axios');
+
+const feathersClient = require('./services/feathersSocketClient');
+//
+//
+//
+// auth on start
+//
+//
+
+const login = async (phone, password) => {
+  try {
+    const res = await feathersClient.authenticate({
+      strategy: 'local',
+      phone,
+      password,
+    });
+    console.log('login res', res);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const isNewUser = async (phoneNumber, countryCode) => {
+  // Sign up - step 1 check for unique phone num
+  // if true: twillio will send you sms with code
+  try {
+    const user = await UserService.find(
+      paramsForServer({
+        query: {
+          phoneNumber,
+          countryCode,
+        },
+        action: 'phone-sign-up',
+      }),
+    );
+
+    // Return Object
+    // @Return - { total: 0, limit: 10, skip: 0, data: [] }
+    if (user.total > 0) {
+      console.log('user already exists', user.data);
+    } else {
+      console.log('This is a new user');
+    }
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+//
+
+const verifyPhone = async (phoneNumber, countryCode, verifyCode) => {
+  try {
+    const res = await axios.post('http://localhost:3030/verify-phone', {
+      phoneNumber,
+      countryCode,
+      verifyCode,
+    });
+    console.log('verify response', res.data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//
+
+const createUser = async (phoneNumber, countryCode, name, password) => {
+  try {
+    const user = await UserService.create(
+      {
+        phoneNumber,
+        countryCode,
+        name,
+        password,
+        roles: ['teacher'],
+      },
+      paramsForServer({ action: 'phone-sign-up' }),
+    );
+
+    const signedIn = await feathersClient.authenticate({
+      strategy: 'local',
+      phone: user.phone,
+      password,
+    });
+
+    console.log('User created: ', user);
+    console.log('signed In', signedIn);
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+//
+
+const updateUser = async (userId, data) => {
+  try {
+    // Call once when the app launch
+    await feathersClient.authenticate();
+
+    const user = await UserService.patch(userId, { ...data });
+    console.log('user updated: ', user);
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+//
+//
+//
+//
+
+//
+//
+// isNewUser('96344902', '852');
+// verifyPhone('96344902', '852', '1196');
+// createUser('96344902', '852', 'Thomas', '1234');
+
+updateUser('5b447546eea2b6557b4bcc50', {
+  birthday: new Date(),
+  name: 'John',
+});
+///
+//
+//
+//
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/services/feathersSocketClient.js
@@ -0,0 +1,24 @@
+const feathers = require('@feathersjs/feathers');
+const socketio = require('@feathersjs/socketio-client');
+const io = require('socket.io-client');
+const auth = require('@feathersjs/authentication-client');
+
+// feathers socket client
+const client = feathers();
+const socket = io('http://localhost:3030');
+client.configure(socketio(socket));
+client.configure(
+  auth({
+    // header: 'Authorization', // the default authorization header for REST
+    // prefix: '', // if set will add a prefix to the header value. for example if prefix was 'JWT' then the header would be 'Authorization: JWT eyJ0eXAiOiJKV1QiLCJhbGciOi...'
+    // path: '/authentication', // the server-side authentication service path
+    // jwtStrategy: 'jwt', // the name of the JWT authentication strategy
+    // entity: 'user', // the entity you are authenticating (ie. a users)
+    // service: 'users', // the service to look up the entity
+    storage: window.localStorage,
+    cookie: 'learnla',
+    storageKey: 'learnla',
+  }),
+);
+
+module.exports = client;
