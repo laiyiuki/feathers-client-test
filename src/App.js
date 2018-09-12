bec8a391da4ccb81da3211a381ded8320d29e834
Thomas Lai <laiyiuki@gmail.com>
2018-09-12 19:58:40 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -52,6 +52,31 @@ class App extends Component {
         profile: auth.user,
       });
 
+      // try {
+      //   const approve = await feathersClient.service('teachers').patch(
+      //     '5b97512673f8bb00150bc590',
+      //     {
+      //       verifications: [
+      //         {
+      //           status: 'approved',
+      //           _id: '5b98a8874b7e200015190355',
+      //           type: 'id',
+      //           image:
+      //             'v1536731270/apps/staging/teachers/verifications/dobfkim9t7jo4rwfn3wx',
+      //         },
+      //       ],
+      //     },
+      //     paramsForServer({
+      //       query: {},
+      //       action: 'verification-approval',
+      //       subdocumentId: '5b98a8874b7e200015190355',
+      //     }),
+      //   );
+      //   console.log('approve', approve);
+      // } catch (err) {
+      //   console.log('err', err);
+      // }
+
       // const approve = await feathersClient.service('teachers').patch(
       //   '5b97512673f8bb00150bc590',
       //   {},
--- src/v2/approval.js
+++ src/v2/approval.js
@@ -5,7 +5,12 @@ const verificationApproval = async () => {
     const approve = await feathersClient.service('teachers').patch(
       '5b97512673f8bb00150bc590',
       {
-        // set status
+        verifications: [{
+            "status" : "approved",
+            "_id" : ObjectId("5b98a8874b7e200015190355"),
+            "type" : "id",
+            "image" : "v1536731270/apps/staging/teachers/verifications/dobfkim9t7jo4rwfn3wx"
+        }]
       },
       paramsForServer({
         query: {},
